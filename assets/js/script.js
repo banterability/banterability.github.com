724fdf8dd04ea205e76a3dc0ccfa9cc1e78262dc
Jeffrey Long <hello@banterability.com>
2011-02-27 23:46:20 -0800

    remove unneeded js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,4 @@
 (function() {
-  $("#projects li").click(function() {
-    var canvas, caption, image, self;
-    self = $(this);
-    canvas = $("#project-canvas");
-    caption = self.data("caption");
-    image = self.data("image");
-    if (self.is('.selected')) {
-      canvas.slideUp();
-      return self.removeClass('selected');
-    } else {
-      $("#projects li").removeClass("selected");
-      self.addClass("selected");
-      if (canvas.is(":visible")) {
-        canvas.slideUp(function() {
-          canvas.find("#canvas-wrapper").html("<img src='" + image + "' /><p class='caption'>" + caption + "</p>");
-          return canvas.slideDown();
-        });
-      } else {
-        canvas.find("#canvas-wrapper").html("<img src='" + image + "' /><p class='caption'>" + caption + "</p>");
-        canvas.slideDown();
-      }
-    }
-  });
   $("#elsewhere li > a").hover(function() {
     var caption, self, title;
     self = $(this);
